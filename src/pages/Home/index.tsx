04fc3cd8af4c81b05dd53596d9a755fa8dc021e2
Gabriel Domingos <gabrielppd77@outlook.com>
2024-08-22 21:43:02 -0300

    start creation of card

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -51,11 +51,33 @@ export default function Home() {
         indexSelected={indexSelected}
         onChangeSelected={setIndexSelected}
       />
-      <div>
-        <h3>Nome do Produto</h3>
-        <h5>Descrição do Produto</h5>
-        <image />
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 p-4">
+        <></>
+
+        <div className="shadow p-4 flex justify-between h-[162px] sm:h-[172px]">
+          <div className="flex flex-col justify-between h-full">
+            <div className="flex flex-col mb-5 flex-1">
+              <h3 className="text-gray-700 font-medium mb-4">
+                Nome do Produto
+              </h3>
+              <span className="text-gray-500 text-sm h-6 font-thin mb-2.5 max-h-max">
+                Descrição do Produto Descrição do Produto Descrição do Produto
+                Descrição do Produto Descrição do Produto Descrição do Produto
+                Descrição do Produto Descrição do Produto
+              </span>
+            </div>
+            <span className="text-gray-700 font-medium">R$ 55,99</span>
+          </div>
+          <div className="w-[120px] h-[120px] sm:w-[140px] sm:h-[140px]">
+            <img
+              alt="Nome Produto"
+              src="https://static.ifood-static.com.br/image/upload/t_low/pratos/4b154a0f-7489-4164-a281-2ad3a9d5009b/202308291821_U3T2_i.jpg"
+            />
+          </div>
+        </div>
       </div>
+
+      <></>
     </main>
   );
 }
