ad6d5ea76880003afe6c7a912709711f3c7adcca
Gabriel Domingos <gabrielppd77@outlook.com>
2024-08-29 22:14:13 -0300

    start development of focus when touch in category

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,17 +1,39 @@
+import { useEffect, useRef } from "react";
+
 import AppBar from "@components/AppBar";
 import ProductCard from "@components/ProductCard";
 
 import useMainContext from "@hooks/useMainContext";
 
 export default function Home() {
-  const { data } = useMainContext();
+  const { categoryIndex, data } = useMainContext();
+
+  const headerRef = useRef<HTMLDivElement | null>(null);
+  const categoryRefs = useRef<{ [key: number]: HTMLDivElement | null }>({});
+
+  //TODO: fix animation appbar
+  //TODO: fix height top animation
+
+  useEffect(() => {
+    if (headerRef.current && categoryRefs.current[categoryIndex]) {
+      categoryRefs.current[categoryIndex]?.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+      });
+    }
+  }, [categoryIndex]);
 
   return (
     <main>
-      <AppBar />
+      <div ref={headerRef}>
+        <AppBar />
+      </div>
       <div className="flex flex-col gap-2 p-4 mt-28">
-        {data.map((category) => (
-          <div key={category.id}>
+        {data.map((category, index) => (
+          <div
+            key={category.id}
+            ref={(el) => (categoryRefs.current[index] = el)}
+          >
             <h2 className="text-gray-700 font-medium text-lg line-clamp-2 mb-2">
               {category.name}
             </h2>
